ded31d680259d670be66380bd0ce5e6dadd4c951
deltea <thcheetah777@gmail.com>
2026-06-08 16:54:32 -0700

    draw a single circle

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
 import "./style.css";
+import p5 from "p5";
+
+const WIDTH = 640 / 4;
+const HEIGHT = 640 / 4
+
+const sketch = (p: p5) => {
+  p.setup = () => {
+    const canvas = p.createCanvas(WIDTH, HEIGHT, p.WEBGL);
+  }
+
+  p.draw = () => {
+    p.background(0);
+
+    p.noSmooth();
+    p.circle(0, 0, 16);
+  }
+}
+
+new p5(sketch);
