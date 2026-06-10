48852e25e4e8ad6c685b3cfe8decf0ef0a7212ed
deltea <thcheetah777@gmail.com>
2026-06-10 15:38:50 -0700

    angle constraints (sorta)

--- src/snake.ts
+++ src/snake.ts
@@ -1,11 +1,11 @@
 import p5 from "p5";
 
 import { Point } from "./point";
-import { Vector } from "./utils";
+import { radToDeg, Vector } from "./utils";
 import { Wave } from "./wave";
 
 const SEGMENT_DISTANCE = 3;
-const SEGMENT_MIN_ANGLE = 120;
+const SEGMENT_MIN_ANGLE = 130;
 
 class Segment extends Point {
   radius: number;
@@ -89,6 +89,28 @@ export class Snake {
     //     segAfter.pos = segAfter.pos.rotate(SEGMENT_MIN_ANGLE - (segBefore.pos.sub(seg.pos).angle() - segAfter.pos.sub(seg.pos).angle()));
     //   }
     // }
+
+    for (let i = 1; i < this.segments.length - 1; i++) {
+      const segBefore = this.segments[i - 1];
+      const seg = this.segments[i];
+      const segAfter = this.segments[i + 1];
+
+      const dirBefore = segBefore.pos.sub(seg.pos).normalize();
+      const dirAfter = segAfter.pos.sub(seg.pos).normalize();
+
+      // dot product gives cos of angle between the two arms
+      const dot = dirBefore.x * dirAfter.x + dirBefore.y * dirAfter.y;
+      const clampedDot = Math.max(-1, Math.min(1, dot));
+      const angleBetween = radToDeg(Math.acos(clampedDot));
+
+      if (angleBetween < SEGMENT_MIN_ANGLE) {
+        const correction = SEGMENT_MIN_ANGLE - angleBetween;
+        // determine rotation direction via cross product
+        const cross = dirBefore.x * dirAfter.y - dirBefore.y * dirAfter.x;
+        const sign = cross >= 0 ? 1 : -1;
+        segAfter.pos = seg.pos.add(segAfter.pos.sub(seg.pos).rotate(sign * correction));
+      }
+    }
 
     // make the head follow the mouse if not close enough already
     const diff = mousePos.sub(this.getHead().pos);
